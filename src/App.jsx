9c183ed4378eda3e609a9dbbc2606df48fdc21bf
Paul Kröner <paulfriedrich.kroener@gmail.com>
2024-04-07 13:39:24 +0200

    navbar and assets

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css';
+import Nav from './components/Nav/nav';
+
+function App() {
+  return (
+    <>
+      <Nav />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Nav/nav.jsx
@@ -0,0 +1,22 @@
+import './nav.css';
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Nav = () => {
+    return (
+        <nav className="nav">
+            <div className="navbar-logo">
+                <img src="../../assets/Logo.svg" alt="logo" />
+            </div>
+            <ul>
+                <Link to="/">Home</Link>
+                <Link to="/menu">Menu</Link>
+                <Link to="/reservation">Reservation</Link>
+                <Link to="/orderonline">Order Online</Link>
+                <Link to="/about">About</Link>
+            </ul>
+        </nav>
+    );
+};
+
+export default Nav;
