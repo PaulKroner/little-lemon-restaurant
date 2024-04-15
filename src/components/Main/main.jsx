7f539ae1393f18606e42c4dce2aeb370dece8f0e
Paul K <paulfriedrich.kroener@gmail.com>
2024-04-15 17:01:52 +0200

    style changed

--- src/components/Main/main.jsx
+++ src/components/Main/main.jsx
@@ -1,11 +1,39 @@
-
+import React from 'react';
+import './main.css';
+import { useNavigate } from "react-router-dom";
+import Menu from './Menu/menu';
 
 const Main = () => {
-    return (
-      <header>
-        <h1>Main</h1>
-      </header>
-    );
-  };
-  
-  export default Main;
+
+  const navigate = useNavigate();
+
+  const ReserveTable = () => {
+    navigate('/reservation');
+  }
+
+
+  return (
+    <div>
+      <div className="main-layout">
+        <header className="main-header">
+          <h1>Little Lemon Restaurant</h1>
+        </header>
+
+        <span className="main-text-content">
+          Lorem ipsum dolor sit amet, consetetur sadipscing elitr,
+          sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat,
+          sed diam voluptua.
+          At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren,
+        </span>
+
+        <button className="main-button" onClick={ReserveTable}>
+          Reserve a table
+        </button>
+      </div>
+
+    <Menu />
+    </div>
+  );
+};
+
+export default Main;
--- src/components/Reservations/confirmed-booking.jsx
+++ src/components/Reservations/confirmed-booking.jsx
@@ -1,10 +1,13 @@
 import bookingConfirmed from '../../assets/bookingConfirmed.png';
+import "./confirmed-booking.css";
 
 const ConfirmedBooking = () => {
     return (
-        <div>
-            <img src={bookingConfirmed} alt="" />
-            <h1>Your Booking is confirmed</h1>
+        <div className="confirmed-booking-layout">
+            <div className="confirmed-booking-sign">
+                <img src={bookingConfirmed} alt="" />
+                <h1 className="confirmed">Your Booking is confirmed</h1>
+            </div>
         </div>
     );
 }
