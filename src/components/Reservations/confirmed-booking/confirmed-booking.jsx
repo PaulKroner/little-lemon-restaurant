eebbcd0ab73f3c6b06f65b7122362d98ff8eb655
Paul Kröner <paulfriedrich.kroener@gmail.com>
2024-04-18 12:04:46 +0200

    confirmed-booking corrected

--- src/components/Reservations/confirmed-booking/confirmed-booking.jsx
+++ src/components/Reservations/confirmed-booking/confirmed-booking.jsx
@@ -4,7 +4,7 @@ import "./confirmed-booking.css";
 const ConfirmedBooking = () => {
     return (
         <div className="confirmed-booking-layout">
-            <div className="confirmed-booking-sign">
+            <div className="confirmed-booking-sign d-flex justify-content-center align-items-center">
                 <img src={bookingConfirmed} alt="" />
                 <h1 className="confirmed">Your Booking is confirmed</h1>
             </div>
